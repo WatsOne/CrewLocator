3642df9d66f62f9be66be81679c8d7f51ca956c4
WatsOne <thewato@gmail.com>
2016-07-02 00:34:33 +0500

    prepare2

---
+++ app/src/main/java/ru/alexkulikov/crewlocator/AsyncHttpPost.java
@@ -0,0 +1,86 @@
+package ru.alexkulikov.crewlocator;
+
+/**
+ * Created by BigWats on 02.07.2016.
+ */
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class AsyncHttpPost extends AsyncTask<String,String,String>
+{
+    private Activity source;
+
+    public AsyncHttpPost(Activity source) {
+        this.source = source;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        BufferedWriter writer = null;
+        OutputStream outputStream = null;
+
+        try {
+            URL url = new URL("https://dzzzr-bot.azurewebsites.net/api/message/gps/");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            outputStream = conn.getOutputStream();
+            writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+            StringBuilder paramBuilder = new StringBuilder();
+            paramBuilder.append(URLEncoder.encode("GpsMessage", "UTF-8"));
+            paramBuilder.append("=");
+            paramBuilder.append(URLEncoder.encode(params[0], "UTF-8"));
+
+            writer.write(paramBuilder.toString());
+            writer.flush();
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != HttpsURLConnection.HTTP_OK) {
+                Toast.makeText(source, "При отправке координат произошла ошибка", Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException ignored) {
+
+                }
+            }
+
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ignored) {
+
+                }
+            }
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/ru/alexkulikov/crewlocator/MainActivity.java
+++ app/src/main/java/ru/alexkulikov/crewlocator/MainActivity.java
@@ -10,40 +10,67 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.security.Provider;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private MyLocationListener locationListener;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            LocationListener locationListener = new MyLocationListener(getBaseContext());
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-            Location loc = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), false));
-            if (loc == null) {
-                Toast.makeText(this, "ЛОСОСНИ", Toast.LENGTH_LONG).show();
-                return;
-            }
-            Toast.makeText(this,
-                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
-                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "ВКЛЮЧИ ЖПС", Toast.LENGTH_LONG).show();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Дайте права на слежение", Toast.LENGTH_SHORT).show();
+            return;
         }
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener(this);
+
+        startButton = (Button) findViewById(R.id.start_btn);
+        assert startButton != null;
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processLocation();
+            }
+        });
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void processLocation() {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Включите GPS", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String lat = "";
+        String lon = "";
+
+        Location lastLocationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location lastLocationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (lastLocationGPS != null) {
+            lat = String.valueOf(lastLocationGPS.getLatitude());
+            lon = String.valueOf(lastLocationGPS.getLongitude());
+        }
+        if (lastLocationNet != null) {
+            lat = String.valueOf(lastLocationNet.getLatitude());
+            lon = String.valueOf(lastLocationNet.getLongitude());
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        String json = "{\"client_id\":\"SPIRT\",\"date\": \"2016-07-01T19:17:49.2662422+00:00\",\"latitude\":"+lat+",\"longitude\":"+lon+"}";
+
+        AsyncHttpPost asyncHttpPost = new AsyncHttpPost(this);
+        asyncHttpPost.execute(json);
     }
 }
